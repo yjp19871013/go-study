5982ba8904d018525a66f7cd94cfe37e821c7c13
Jiapeng Yin <yjp19871013@126.com>
2019-04-19 13:57:37 +0800

    Update msg_queue.go

--- msg_queue/msg_queue.go
+++ msg_queue/msg_queue.go
@@ -3,12 +3,14 @@ package msg_queue
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 )
 
 type MsgQueueHandler interface {
-	OnStart()
-	OnStop()
-	OnMsgRecv(msg *Message)
+	OnStart(q *MsgQueue)
+	OnStop(q *MsgQueue)
+	OnMsgRecv(q *MsgQueue, msg *Message)
+	OnDefaultRun(q *MsgQueue)
 }
 
 type Message struct {
@@ -17,10 +19,15 @@ type Message struct {
 }
 
 func formMessage(msg string, data interface{}) (*Message, error) {
-	jsonStr, err := json.Marshal(data)
-	if err != nil {
-		fmt.Println("SendMsg error", err.Error())
-		return nil, err
+	jsonStr := make([]byte, 0)
+	if data != nil {
+		dataJson, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println("formMessage error", err.Error())
+			return nil, err
+		}
+
+		jsonStr = dataJson
 	}
 
 	message := new(Message)
@@ -33,18 +40,32 @@ func formMessage(msg string, data interface{}) (*Message, error) {
 type MsgQueue struct {
 	handler MsgQueueHandler
 
+	observers      []*MsgQueue
+	observersMutex sync.Mutex
+
 	msgChan  chan *Message
 	stopChan chan bool
 }
 
 func InitMsgQueue(handler MsgQueueHandler) *MsgQueue {
+	return InitMsgQueueWithMsgBufferSize(handler, 0)
+}
+
+func InitMsgQueueWithMsgBufferSize(handler MsgQueueHandler, bufferSize int) *MsgQueue {
 	if handler == nil {
 		return nil
 	}
 
 	q := new(MsgQueue)
 	q.handler = handler
-	q.msgChan = make(chan *Message)
+	q.observers = make([]*MsgQueue, 0)
+
+	if bufferSize > 0 {
+		q.msgChan = make(chan *Message, bufferSize)
+	} else {
+		q.msgChan = make(chan *Message)
+	}
+
 	q.stopChan = make(chan bool)
 
 	return q
@@ -57,6 +78,7 @@ func DestroyMsgQueue(q *MsgQueue) {
 
 	q.stopChan = nil
 	q.msgChan = nil
+	q.observers = nil
 	q.handler = nil
 	q = nil
 }
@@ -67,6 +89,43 @@ func (q *MsgQueue) Start() {
 
 func (q *MsgQueue) Stop() {
 	q.stopChan <- true
+	<-q.stopChan
+}
+
+func (q *MsgQueue) AddObserver(observer *MsgQueue) {
+	if observer == nil {
+		return
+	}
+
+	q.observersMutex.Lock()
+	defer q.observersMutex.Unlock()
+
+	q.observers = append(q.observers, observer)
+}
+
+func (q *MsgQueue) DeleteObserver(observer *MsgQueue) {
+	if observer == nil {
+		return
+	}
+
+	q.observersMutex.Lock()
+	defer q.observersMutex.Unlock()
+
+	for i, o := range q.observers {
+		if o == observer {
+			q.observers = append(q.observers[:i], q.observers[i+1:]...)
+			break
+		}
+	}
+}
+
+func (q *MsgQueue) NotifyObservers(msg string, data interface{}) {
+	q.observersMutex.Lock()
+	defer q.observersMutex.Unlock()
+
+	for _, o := range q.observers {
+		o.SendMsg(msg, data)
+	}
 }
 
 func (q *MsgQueue) SendMsg(msg string, data interface{}) {
@@ -79,24 +138,21 @@ func (q *MsgQueue) SendMsg(msg string, data interface{}) {
 }
 
 func (q *MsgQueue) run() {
-	q.handler.OnStart()
+	q.handler.OnStart(q)
 
 	for {
 		select {
 		case stop := <-q.stopChan:
 			if stop {
-				q.handler.OnStop()
+				q.handler.OnStop(q)
+				q.stopChan <- true
+				return
 			}
+			q.stopChan <- true
 		case msg := <-q.msgChan:
-			q.dealWithMsg(msg)
+			q.handler.OnMsgRecv(q, msg)
+		default:
+			q.handler.OnDefaultRun(q)
 		}
 	}
-}
-
-func (q *MsgQueue) dealWithMsg(msg *Message) {
-	if msg == nil {
-		return
-	}
-
-	q.handler.OnMsgRecv(msg)
 }
